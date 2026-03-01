624f7a8f340a26e4e2aa4d776610f4f9ce052b2d
Dhruv-Adhiya <dhruvadhiya230407@gmail.com>
2026-03-01 20:16:39 +0530

    make some changes in server.js file

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,30 @@
 require('dotenv').config();
+const express = require("express");
 const pool = require("./config/db");
 
+const app = express();
+const PORT = 5000;
+
+// Test DB connection when server starts
 pool.query("SELECT NOW()", (err, res) => {
   if (err) {
     console.error("DB connection failed", err);
   } else {
-    console.log("DATABASE_URL:", process.env.DATABASE_URL);
     console.log("DB connected:", res.rows[0]);
-    
   }
+});
+
+// Create a test route
+app.get("/", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.json({ time: result.rows[0] });
+  } catch (error) {
+    res.status(500).json({ error: "Database error" });
+  }
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
