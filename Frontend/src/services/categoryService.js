38c62810d04246f6d37cebf267efdb3a14eae7bf
Dhruv Adhiya <dhruvadhiya230407@gmail.com>
2026-09-17 13:33:13 +0530

    Delete Frontend directory

--- Frontend/src/services/categoryService.js
+++
@@ -1,9 +0,0 @@
-import api from './api';
-
-export const getCategories = () => api.get('/categories').then((r) => r.data.data);
-
-export const createCategory = (data) => api.post('/categories', data).then((r) => r.data.data);
-
-export const deleteCategory = (id) => api.delete(`/categories/${id}`).then((r) => r.data);
-
-export const updateCategory = (id, data) => api.put(`/categories/${id}`, data).then((r) => r.data.data);
